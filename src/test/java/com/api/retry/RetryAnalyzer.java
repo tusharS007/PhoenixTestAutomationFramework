d67dc410f6d4e3f3c70e3ded9f04ac9e72c3b783
Tushar D Shelar <09tusharshelar@gmail.com>
2026-03-05 17:49:55 +0530

    Added Logger to the Retry Analyzer

--- src/test/java/com/api/retry/RetryAnalyzer.java
+++ src/test/java/com/api/retry/RetryAnalyzer.java
@@ -1,17 +1,23 @@
 package com.api.retry;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
 
+	private static final Logger LOGGER = LogManager.getLogger(RetryAnalyzer.class);
 	private static final int MAX_ATTEMPS = 2;
 	private int count = 1;
 
 	@Override
 	public boolean retry(ITestResult result) {
 
+		LOGGER.info("Checking if the {} test can be re-executed", result.getName());
 		if (count <= MAX_ATTEMPS) {
+			LOGGER.warn("Executing the {} test, Current Attempt: {}/{} REASON{} ", result.getName(), count, MAX_ATTEMPS,
+					result.getThrowable().getMessage());
 			count++;
 			return true;
 		}
